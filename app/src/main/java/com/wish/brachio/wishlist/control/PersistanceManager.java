7341608d6327d7c93d9ccb8d422a9bcf768b797e
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-11-03 19:53:38 -0400

    Added HubActivity

--- app/src/main/java/com/wish/brachio/wishlist/control/PersistanceManager.java
+++ app/src/main/java/com/wish/brachio/wishlist/control/PersistanceManager.java
@@ -3,11 +3,16 @@ package com.wish.brachio.wishlist.control;
 import android.app.Activity;
 
 import com.wish.brachio.wishlist.model.FirebaseUserHandler;
+import com.wish.brachio.wishlist.model.User;
 
 public class PersistanceManager {
 
     public void signIn(String email, String password, Activity activity){
         FirebaseUserHandler handler = new FirebaseUserHandler();
         handler.signIn(email, password, activity);
+    }
+
+    public void registerUser(User user){
+
     }
 }
