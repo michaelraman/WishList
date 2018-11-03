a54efbadcaa1bebaa13d5d2f5425a95224fabf64
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-11-03 19:37:13 -0400

    Added Item, User, Wishlist classes.

--- app/src/main/java/com/wish/brachio/wishlist/model/FirebaseUserHandler.java
+++ app/src/main/java/com/wish/brachio/wishlist/model/FirebaseUserHandler.java
@@ -1,4 +1,8 @@
 package com.wish.brachio.wishlist.model;
 
 public class FirebaseUserHandler {
+
+    public void signIn(String email, String password){
+
+    }
 }
---
+++ app/src/main/java/com/wish/brachio/wishlist/model/User.java
@@ -0,0 +1,4 @@
+package com.wish.brachio.wishlist.model;
+
+public class User {
+}
