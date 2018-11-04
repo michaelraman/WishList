dec237b6bbff8528d7a583e5f9dcefc2fb928e9a
DESKTOP-2NIVFK9\rachel <tierne1t@gmail.com>
2018-11-04 06:32:14 -0500

    Added UserItemListAdapater for YourWishlistActivity

--- app/src/main/java/com/wish/brachio/wishlist/YourWishlistActivity.java
+++ app/src/main/java/com/wish/brachio/wishlist/YourWishlistActivity.java
@@ -6,18 +6,22 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.wish.brachio.wishlist.control.PersistanceManager;
 import android.widget.Adapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.wish.brachio.wishlist.model.Item;
-import com.wish.brachio.wishlist.model.adapter.UserItemListAdapter;
+
 
 import java.util.ArrayList;
 
@@ -42,17 +46,25 @@ public class YourWishlistActivity extends AppCompatActivity {
         });
         */
 
-        RecyclerView list = findViewById( R.id.your_wishlist );
+        RecyclerView recyclerView = findViewById( R.id.your_wishlist );
         ArrayList<Item> items = new ArrayList();
         Item newItem = new Item("somename");
         items.add(newItem);
-        UserItemListAdapter adapter = (UserItemListAdapter) new UserItemListAdapter( items, new UserItemListAdapter.OnItemClickListener() {
+
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager  layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        UserItemListAdapter adapter = new YourWishlistActivity.UserItemListAdapter(items, new UserItemListAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(Item item) {
-
+                
             }
-        } );
-        list.setAdapter( adapter);
+        });
+
+        recyclerView.setAdapter( adapter );
 
     }
 
@@ -60,6 +72,98 @@ public class YourWishlistActivity extends AppCompatActivity {
         Log.e("pls","Opening friends' lists");
         Intent intent = new Intent(this, AddingItems.class);
         startActivity(intent);
+    }
+
+    private static  class UserItemListAdapter extends RecyclerView.Adapter<YourWishlistActivity.UserItemListAdapter.ItemViewHolder>{
+        private ArrayList<Item> items;
+        private View view;
+        private final YourWishlistActivity.UserItemListAdapter.OnItemClickListener theItemListener;
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public static class ItemViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            private TextView nameText;
+            private TextView contributor;
+            private View v;
+            public ItemViewHolder(View v) {
+                super(v);
+                this.v = v;
+                nameText = (TextView) v.findViewById(R.id.item_name_adapter);
+                contributor = (TextView) v.findViewById(R.id.item_contributor);
+
+            }
+
+            public void bind(final Item theItem, final YourWishlistActivity.UserItemListAdapter.OnItemClickListener listener) {
+                v.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("Wish", "To Item");
+                        listener.onItemClick(theItem);
+                    }
+                });
+            }
+        }
+
+        public interface OnItemClickListener {
+            void onItemClick(Item item);
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public UserItemListAdapter(ArrayList<Item> array, YourWishlistActivity.UserItemListAdapter.OnItemClickListener listener) {
+            this.items = array;
+            this.theItemListener = listener;
+        }
+
+        // Create new views (invoked by the layout manager)
+        // Create new views (invoked by the layout manager)
+        @Override
+        public YourWishlistActivity.UserItemListAdapter.ItemViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                                                             int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_adapter, parent, false);
+
+            view = v;
+            YourWishlistActivity.UserItemListAdapter.ItemViewHolder vh = new YourWishlistActivity.UserItemListAdapter.ItemViewHolder(v);
+            return vh;
+        }
+
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(YourWishlistActivity.UserItemListAdapter.ItemViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.nameText.setText(items.get(position).getItemName());
+            String c = items.get(position).getContributers();
+            if (c != null){
+                holder.contributor.setText(c);
+            } else {
+                holder.contributor.setText("None.  Click to add yourself as a contributor.");
+            }
+
+            holder.bind(items.get(position), theItemListener);
+//            view.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    //TODO add intent to next page
+//                }
+//            });
+
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
     }
 
 }
