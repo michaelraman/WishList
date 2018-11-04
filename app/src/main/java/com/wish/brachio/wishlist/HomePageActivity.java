6547c55a2959489d013fd23b1d9cca766116ae94
Tushna Eduljee <tushnae@gmail.com>
2018-11-04 03:46:08 -0500

    Tushna's changes

--- app/src/main/java/com/wish/brachio/wishlist/HomePageActivity.java
+++ app/src/main/java/com/wish/brachio/wishlist/HomePageActivity.java
@@ -19,10 +19,10 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        setContentView(R.layout.content_home_page);
+       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+/*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +30,7 @@ public class HomePageActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         ImageButton mCreateWishList = (ImageButton) findViewById(R.id.imageButton5);
         mCreateWishList.setOnClickListener(new View.OnClickListener() {
